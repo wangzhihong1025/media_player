34e1fdb762a5a29660a996ba441bdc945a80ea29
wangzhihong <wang>
2016-09-29 21:50:56 +0800

    设置视频进度条按下和拖拽及视频音量的按下和拖拽，视频点击暂停播放

--- media_player.js
+++ media_player.js
@@ -1,15 +1,32 @@
 (function() {
+    var input = document.getElementById('input');
+    var btn = document.getElementById('btn');
+    add_class.onclick = function() {
+        var file = input.files[0];
+        var url = URL.createObjectURL(file);
+        video.src = url;
+    }
     var video = document.getElementById('video');
     var screen_pause = document.getElementById('screen_pause');
     var play = document.getElementById('play');
     var pause = document.getElementById('pause');
     var all = document.getElementById('dilanweizhi');
     var bg = document.getElementById('bg');
-    bg.onmouseover = function () {
+    bg.onmouseover = function() {
         all.style.display = "block";
     }
-    bg.onmouseout = function () {
+    bg.onmouseout = function() {
         all.style.display = "none";
+    }
+    video.poster = "img/poster.jpg";
+    video.onclick = function() {
+        if (video.paused == true) {
+            video.play();
+            screen_pause.style.display = "none";
+        } else {
+            video.pause();
+            screen_pause.style.display = "block";
+        }
     }
     pause.onclick = function() {
         screen_pause.style.display = "none";
@@ -49,11 +66,38 @@
         var baifenbi = video.currentTime / video.duration;
         var jinduball = document.getElementById('jinduball');
         var jindutiao = document.getElementById('jindutiao');
-        jindutiao.style.width = baifenbi * 480 +"px";
-        jinduball.style.left = baifenbi * 480 + 130 +"px";
+        jindutiao.style.width = baifenbi * 480 + "px";
+        jinduball.style.left = baifenbi * 480 + 130 + "px";
 
     }
-    video.onended = function () {
+
+    jindu.onmousedown = function(event) {
+        var length = event.clientX - jindutiao.offsetLeft;
+        var percent = (length - 10) / jindu.offsetWidth;
+        jindutiao.style.width = percent * 480 + "px";
+        video.currentTime = percent * video.duration;
+    }
+    jindutiao.onmousedown = function(event) {
+        var length = event.clientX - jindutiao.offsetLeft;
+        var percent = (length - 10) / jindu.offsetWidth;
+        jindutiao.style.width = percent * 480 - 140 + "px";
+        video.currentTime = percent * video.duration;
+    }
+    jinduball.ondragend = function(event) {
+        var length = event.clientX - jindu.offsetLeft;
+        var percent = (length - 10) / jindu.offsetWidth;
+        jindutiao.style.width = percent * 480 + "px";
+        jinduball.style.left = percent * 480 + 140 + "px";
+        video.currentTime = percent * video.duration;
+    }
+    jinduball.ondrag = function(event) {
+        var length = event.clientX - jindu.offsetLeft;
+        var percent = (length - 10) / jindu.offsetWidth;
+        jindutiao.style.width = percent * 480 + "px";
+        jinduball.style.left = percent * 480 + 140 + "px";
+        video.currentTime = percent * video.duration;
+    }
+    video.onended = function() {
         play.style.display = "none";
         pause.style.display = "block";
     }
@@ -69,6 +113,40 @@
         sound.style.display = "block";
         mute.style.display = "none";
     }
-    
 
+    var volume = document.getElementById('volume');
+    var volumemove = document.getElementById('volumemove');
+    var volumeball = document.getElementById('volumeball');
+    volumeball.ondragend = function(event) {
+        var length = event.clientX - volumeball.offsetLeft;
+        var percent = length / volume.offsetWidth;
+        if (percent >= 0 && percent <= 1) {
+            volumemove.style.width = percent * 100 + "px";
+            volumeball.style.left = percent * 100 + 680 + "px";
+            video.volume = percent;
+        }
+    }
+    volumeball.ondrag = function(event) {
+        var length = event.clientX - volumeball.offsetLeft;
+        var percent = length / volume.offsetWidth;
+        if (percent >= 0 && percent <= 1) {
+            volumemove.style.width = percent * 100 + "px";
+            volumeball.style.left = percent * 100 + 680 + "px";
+            video.volume = percent;
+        }
+    }
+    volume.onmousedown = function(event) {
+        var length = event.clientX - volumemove.offsetLeft;
+        var percent = (length - 10) / volume.offsetWidth;
+        volumemove.style.width = percent * 100 + "px";
+        volumeball.style.left = percent * 100 + 670 + "px";
+        video.volume = percent;
+    }
+    volumemove.onmousedown = function(event) {
+        var length = event.clientX - volumemove.offsetLeft;
+        var percent = (length - 10) / volume.offsetWidth;
+        volumemove.style.width = percent * 100 + "px";
+        volumeball.style.left = percent * 100 + 670 + "px";
+        video.volume = percent;
+    }
 }(window))
